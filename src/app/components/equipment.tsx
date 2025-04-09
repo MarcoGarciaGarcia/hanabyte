a40316795eb008fd41cc3ae3e5c40cf8c77fddc7
MarcoGarciaGarcia <marcogarciag1403@gmail.com>
2025-04-08 20:01:17 -0600

    social media links

--- src/app/components/equipment.tsx
+++ src/app/components/equipment.tsx
@@ -12,6 +12,8 @@ const EquipmentSection: React.FC = () => {
       descripcion:
         "Encargado de diseñar y mantener la infraestructura de la plataforma, asegurando que las aplicaciones sean escalables, eficientes y seguras.",
       imgSrc: "/e2.jpg",
+      facebook: "https://www.facebook.com/share/1UM2aGnGbg/",
+      github: "https://github.com/AgustinEscobedo",
     },
     {
       nombre: "Marco García",
@@ -19,6 +21,8 @@ const EquipmentSection: React.FC = () => {
       descripcion:
         "Responsable de crear y dar mantenimiento interfaces intuitivas y atractivas, asegurando una experiencia de usuario fluida y responsive en la plataforma.",
       imgSrc: "/e1.jpg",
+      facebook: "https://www.facebook.com/share/1ATH1hj3eN/",
+      github: "https://github.com/MarcoGarciaGarcia",
     },
   ];
 
@@ -26,8 +30,8 @@ const EquipmentSection: React.FC = () => {
   const isInView = useInView(sectionRef, { once: true, amount: 0.2 });
 
   return (
-    <section 
-      ref={sectionRef} 
+    <section
+      ref={sectionRef}
       className="w-full py-10 px-4 sm:px-6 md:px-8 lg:px-12 xl:px-20"
     >
       <div className="max-w-7xl mx-auto">
@@ -48,8 +52,8 @@ const EquipmentSection: React.FC = () => {
             className="text-base sm:text-lg md:text-xl text-gray-600 mx-auto max-w-3xl"
           >
             Nuestro equipo es una mezcla de talento y cercanía, como un campo de
-            cerezos en flor. Cada uno aporta su esencia única, y juntos cultivamos
-            soluciones digitales frescas y auténticas.
+            cerezos en flor. Cada uno aporta su esencia única, y juntos
+            cultivamos soluciones digitales frescas y auténticas.
           </motion.p>
         </div>
 
@@ -89,7 +93,7 @@ const EquipmentSection: React.FC = () => {
                     <ul className="flex justify-center space-x-4">
                       <li>
                         <a
-                          href="#"
+                          href={miembro.facebook}
                           className="text-gray-500 hover:text-[#379aa3] transition-colors"
                         >
                           <svg
@@ -106,24 +110,10 @@ const EquipmentSection: React.FC = () => {
                           </svg>
                         </a>
                       </li>
+
                       <li>
                         <a
-                          href="#"
-                          className="text-gray-500 hover:text-[#379aa3] transition-colors"
-                        >
-                          <svg
-                            className="w-5 h-5"
-                            fill="currentColor"
-                            viewBox="0 0 24 24"
-                            aria-hidden="true"
-                          >
-                            <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
-                          </svg>
-                        </a>
-                      </li>
-                      <li>
-                        <a
-                          href="#"
+                          href={miembro.github}
                           className="text-gray-500 hover:text-[#379aa3] transition-colors"
                         >
                           <svg
